07d31fc4af2d79e6e9e2a994c8f8f9f7612761ba
arufonsekun <junior.ramisch@gmail.com>
2019-11-29 22:18:55 -0300

    creating box world
    is realy fucked up a find a image
    with high resolution and increase
    box size

--- src/js/index-refactor.js
+++ src/js/index-refactor.js
@@ -4,95 +4,69 @@ import { OrbitControls } from '../../lib/three.js-r110/examples/jsm/controls/Orb
 
 let FlappyBird = () => {
 
-    const MODEL_PATH = './src/models/scene.gltf';
-    const GRASS_TEXTURE = './src/textures/grass.jpeg';
     const SKY_TEXTURE = './src/textures/sky.jpeg';
+    const front = './src/textures/plane-box-front.jpg';
+    const back = './src/textures/plane-box-back.jpg';
+    const bottom = './src/textures/plane-box-bottom.jpg';
+    const top = './src/textures/plane-box-top.jpg';
+    const left = './src/textures/plane-box-left.jpg';
+    const right = './src/textures/plane-box-right.jpg';
+
     let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    let camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 45, 30000);
     let renderer = new THREE.WebGLRenderer({antialias:true});
-    let loader = new GLTFLoader();
-    let sun, groundTexture, groundMaterial, groundMesh, groundGeometry;
-    let mousePointerX = 0;
-    let mousePointerY = 0;
-    var birdModelMesh;
     
     let init  = () => {
-        camera.position.set(0,250,100);
+        camera.position.set(-900,-200,-900);
         let controls = new OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', renderer);
+        controls.minDistance = 50;
+        controls.maxDistance = 150;
         renderer.setSize(window.innerWidth, window.innerHeight);
-        scene.background = new THREE.Color(0x0f0f0f);
+        //scene.background = new THREE.Color(0x0f0f0f);
         document.body.appendChild(renderer.domElement);
     }
     
-    let loadModelHandler = (model) => {
-        var birdModelMesh = model.scene.children[0];
-        birdModelMesh.position.z = -200;
-        birdModelMesh.scale.x = 0.51;
-        birdModelMesh.scale.y = 0.51;
-        birdModelMesh.scale.z = 0.51;
-        scene.add(birdModelMesh);
-        sunShine(birdModelMesh);
-    }
-    
-    let loadBirdModel = () => {
-        loader.load(MODEL_PATH, loadModelHandler)
-    }
-    
-    let sunShine = (birdModel) => {
-        sun = new THREE.DirectionalLight(0xffffff, 0.5);
-        sun.position.set( 0, 1, 0 );
-        sun.castShadow = true;
-        sun.target = birdModel;
-        scene.add(sun);
-    }
-    
-    let ambientLight = () => {
-        let light = new THREE.SpotLight(0xffffff, 5, 1000);
-        light.position.set(200, 250, 500);
-        light.angle = 3;
-        light.penumbra = 0.5;
-        
-        light.castShadow = true;
-        light.shadow.mapSize.width = 1024;
-        light.shadow.mapSize.height = 1024;
-        
-        scene.add(light);
-    }
-    
-    let ground = () => {
-        groundTexture = new THREE.TextureLoader().load(GRASS_TEXTURE);
-        groundGeometry = new THREE.PlaneBufferGeometry(2000, 2000);
-        groundMaterial = new THREE.MeshPhongMaterial({color : 0xffffff, map : groundTexture});
-        
-        groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-        
-        groundMesh.rotation.x = - Math.PI / 2;
-        groundMesh.material.map.repeat.set(8, 8);
-        groundMesh.material.map.wrapS = groundMesh.material.map.wrapT = THREE.RepeatWrapping;
-        groundMesh.receiveShadow = true;
-        
-        scene.add(groundMesh);
+    let world = () => {
+        let worldBoxMaterial = [];
+        let textureFT = new THREE.TextureLoader().load(front);
+        let textureBK = new THREE.TextureLoader().load(back);
+        let textureTP = new THREE.TextureLoader().load(top);
+        let textureBT = new THREE.TextureLoader().load(bottom);
+        let textureRT = new THREE.TextureLoader().load(right);
+        let textureLT = new THREE.TextureLoader().load(left);
+
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureFT }));
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureBK }));
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureTP }));
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureBT }));
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureRT }));
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureRT }));
+        worldBoxMaterial.push(new THREE.MeshBasicMaterial({ map : textureLT }));
+
+        let worldBoxGeometry = new THREE.BoxGeometry(100, 100, 100);
+        let boxWorld = new THREE.Mesh(worldBoxGeometry, worldBoxMaterial);
+
+        for (let i=0; i < 6; i++)
+            worldBoxMaterial[i].side = THREE.BackSide
+
+        scene.add(boxWorld);
     }
 
     let animate = () => {
+        renderer.render(scene, camera);
         requestAnimationFrame(animate);
 
-        renderer.render(scene, camera);
     }
 
     return {
         init : () => init(),
-        loadBirdModel : () => loadBirdModel(),
-        sunShine : () => sunShine(),
-        ground : () => ground(),
+        world: () => world(),
         start : () => animate(),
-        ambientLight: () => ambientLight()
     }
 }
 
 let bird = FlappyBird();
 bird.init();
-bird.loadBirdModel();
-bird.ambientLight();
-bird.ground();
+bird.world();
 bird.start();
