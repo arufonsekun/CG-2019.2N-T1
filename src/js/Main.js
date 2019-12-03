d54a3312ee2fc56db665ab4459b017fec538b5f5
arufonsekun <junior.ramisch@gmail.com>
2019-12-03 19:57:15 -0300

    rename some files, a Bird module must be created

--- src/js/Main.js
+++ src/js/Main.js
@@ -2,6 +2,37 @@ import * as THREE from '../../lib/three.js-r110/build/three.module.js';
 import { GLTFLoader } from '../../lib/three.js-r110/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from '../../lib/three.js-r110/examples/jsm/controls/OrbitControls.js';
 
+var Bird = () => {
+
+    let loader = new GLTFLoader();
+    let birdMesh;
+
+    let loadModelHandler = (model) => {
+        let birdModelMesh = model.scene.children[0];
+        birdModelMesh.scale.x = 0.51;
+        birdModelMesh.scale.y = 0.51;
+        birdModelMesh.scale.z = 0.51;
+        birdModelMesh.position.x = -500;
+        scene.add(birdModelMesh);
+        sunShine(birdModelMesh);
+        
+        return birdModelMesh;
+    }
+
+    let loadBirdModel = (modelPath) => {
+        birdMesh = loader.load(modelPath, loadModelHandler)
+    }
+
+    let getBird = () => {
+        return birdMesh;
+    }
+
+    return {
+        getBird : () => getBird(),
+        init : (modelPath) => loadBirdModel(modelPath) 
+    }
+}
+
 let Game = () => {
 
     const MODEL_PATH = './src/models/scene.gltf';
@@ -13,14 +44,10 @@ let Game = () => {
     let loader = new GLTFLoader();
     
     let sun, groundMaterial, groundMesh, groundGeometry;
-    let birdModelMesh, controls;
+    let controls, bird;
     
     let spaceKeyHandler = (e) => {
-        if (e.key == SPACE) {
-            birdModelMesh.position.y += 10;
-            iteration = 1;
-            rotation = -0.01;
-        }
+        if (e.key == SPACE) { console.log("Teclou espaço"); }
     }
 
     let init  = () => {
@@ -30,8 +57,11 @@ let Game = () => {
 
         renderer.setSize(window.innerWidth, window.innerHeight);
         scene.background = new THREE.Color(0x0f0f0f);
+        
         document.body.appendChild(renderer.domElement);
         document.onkeypress = spaceKeyHandler;
+
+        bird = Bird();
     }
     
     let sunShine = (birdModel) => {
@@ -62,27 +92,21 @@ let Game = () => {
 
     let animate = () => {
         requestAnimationFrame(animate);
-        
-        birdModelMesh.position.y -= 1;
 
         renderer.render(scene, camera);
     }
 
     return {
         init : () => init(),
-        loadBirdModel : () => loadBirdModel(),
         sunShine : () => sunShine(),
         ground : () => ground(),
         start : () => animate(),
-        ambientLight: () => ambientLight(),
-        createPipes: () => createPipes()
+        ambientLight: () => ambientLight()
     }
 }
 
 let game = Game();
 game.init();
-game.loadBirdModel();
 game.ambientLight();
 game.ground();
-game.createPipes();
 game.start();
