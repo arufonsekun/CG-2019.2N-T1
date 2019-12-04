58b093bd864ea3444aa014e281f72a13c466ee4d
arufonsekun <junior.ramisch@gmail.com>
2019-12-03 22:12:20 -0300

    Pipe class was created ready to merge this branch
    next step find a pipe texture or 3d model

--- src/js/Main.js
+++ src/js/Main.js
@@ -32,30 +32,59 @@ let Bird = (scene) => {
 
     let loadBirdModel = () =>
     {
-        birdMesh = loader.load(BIRD_MODEL, loadModelHandler);
+        loader.load(BIRD_MODEL, loadModelHandler);
     }
 
     let fall = (y) =>
     {
-        birdMesh.position.y -= y;
+        if (birdMesh) {
+            birdMesh.position.y -= y;
+            birdMesh.rotation.y += 0.1;
+        }
     }
 
     let climb = (y) =>
     {
-        birdMesh.position.y += y;
-    }
-
-    let getBird = () =>
-    {
-        return birdMesh;
+        if (birdMesh) birdMesh.position.y += y;
     }
 
     return {
-        getBird : () => getBird(),
         init : () => loadBirdModel(),
         fall : (y) => fall(y),
         climb : (x) => climb(x)
     }
+}
+
+let Pipe = (scene, opening) => {
+
+    let topPipe, bottomPipe;
+
+    let init = () =>
+    {
+        let geometry = new THREE.CylinderGeometry( 40, 40, 200, 32 );
+        let material = new THREE.MeshBasicMaterial( {color: 0xeeeeee } );
+
+        topPipe = new THREE.Mesh( geometry, material );
+        bottomPipe = new THREE.Mesh( geometry, material );
+
+        bottomPipe.position.y = 100;
+        topPipe.position.y = 300 + opening;
+        
+        scene.add( bottomPipe )
+        scene.add( topPipe );
+    }
+
+    let move = (x) => {
+        topPipe.position.x -= x;
+        bottomPipe.position.x -= x;
+        
+    }
+
+    return {
+        init : () => init(),
+        move : (x) => move(x)
+    }
+
 }
 
 let Game = () => {
@@ -64,40 +93,62 @@ let Game = () => {
 
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer({antialias:true}), bird;
+    let renderer = new THREE.WebGLRenderer({antialias:true});
     
     let sun, groundMaterial, groundMesh, groundGeometry;
-    let controls;
+    let controls, bird, pipe;
     
     let spaceKeyHandler = (e) => {
-        if (e.key == SPACE)
-        { 
+        
+        if (e.key == SPACE) 
+        {
             bird.climb(10);
         }
+
     }
 
-    let init  = () => {
-        
-        camera.position.set(-320.6, 164.2, 541.9);
+    let createControls = () => {
+
         controls = new OrbitControls(camera, renderer.domElement);
 
+    }
+
+    let setDefaultSettings = () => {
+
+        camera.position.set(-320.6, 164.2, 541.9);
         renderer.setSize(window.innerWidth, window.innerHeight);
         scene.background = new THREE.Color(0x0f0f0f);
-        
-        bird = Bird(scene);
-        bird.init();
 
-        document.body.appendChild(renderer.domElement);
+    }
+
+    let addListeners = () => {
+        
         document.onkeypress = spaceKeyHandler;
 
     }
+
+    let createCanvas = () => {
     
-    let sunShine = (birdModel) => {
-        sun = new THREE.DirectionalLight(0xffffff, 0.5);
-        sun.position.set( 0, 1, 0 );
-        sun.castShadow = true;
-        sun.target = birdModel;
-        scene.add(sun);
+       document.body.appendChild(renderer.domElement);
+
+    }
+
+    let init  = () => {
+
+        createControls();
+        
+        setDefaultSettings();
+
+        bird = Bird(scene);
+        bird.init();
+        
+        pipe = Pipe(scene, 100);
+        pipe.init();
+
+        console.log(pipe);
+
+        addListeners();
+        createCanvas();
     }
     
     let ambientLight = () => {
@@ -121,14 +172,14 @@ let Game = () => {
     let animate = () => {
         requestAnimationFrame(animate);
 
-        if (bird) { bird.fall(1.5); }
+        bird.fall(1.5);
+        pipe.move(1.5);
 
         renderer.render(scene, camera);
     }
 
     return {
         init : () => init(),
-        sunShine : () => sunShine(),
         ground : () => ground(),
         start : () => animate(),
         ambientLight: () => ambientLight()
